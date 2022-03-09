e4256fcbf76a2057b942820570be81e180f32e1c
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-03-08 23:24:29 -0600

    start App

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App/Styles/General.css"
+
+const App = () => {
+    return (
+        <div data-scroll-container>
+            <div className="Navbar">
+                
+            </div>
+            <div className="Content ">
+                
+            </div>
+        </div>
+    )
+}
+
+export default App;
