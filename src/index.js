b9fb0f002d3137eda175e472c9c423334eb1e6a8
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-03-10 13:28:36 -0600

    Cambios

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
-      document.getElementById("navbar").style.padding = "20px 10px";
+      document.getElementById("navbar").style.padding = "15px 10px";
       document.getElementById("navbar").style.marginBottom = "-30px";
       document.getElementById("navbar").style.backgroundColor = "rgba(0, 0, 0, 0.9)";
       //document.getElementById("navbar").style.back
       //document.getElementById("navbar").style.color = "white";
       //document.getElementById("logo").style.fontSize = "25px";
     } else {
-      document.getElementById("navbar").style.padding = "50px 10px";
+      document.getElementById("navbar").style.padding = "30px 10px";
       document.getElementById("navbar").style.marginBottom = "-80px"
       document.getElementById("navbar").style.backgroundColor = "transparent"
       //document.getElementById("logo").style.fontSize = "35px";
